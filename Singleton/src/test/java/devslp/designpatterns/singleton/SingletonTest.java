ebce5bdca10be94bcafc22bb2d7e82581dc6462a
Enrique Guardiola <enrique.guardiola@hotmail.com>
2017-03-28 20:20:30 -0600

    Add singleton pattern source files

---
+++ Singleton/src/test/java/devslp/designpatterns/singleton/SingletonTest.java
@@ -0,0 +1,44 @@
+package devslp.designpatterns.singleton;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Enrique Guardiola
+ */
+public class SingletonTest {
+    @Test
+    public void verifyUniqueInstance(){
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+        assert(singleton1 == singleton2);
+    }
+    
+    @Test
+    public void verifyEagerUniqueInstance(){
+        EagerInitializedSingleton singleton1 = EagerInitializedSingleton.getInstance();
+        EagerInitializedSingleton singleton2 = EagerInitializedSingleton.getInstance();
+        assert(singleton1 == singleton2);
+    }
+    
+    @Test
+    public void verifyStaticBlockUniqueInstance(){
+        StaticBlockSingleton singleton1 = StaticBlockSingleton.getInstance();
+        StaticBlockSingleton singleton2 = StaticBlockSingleton.getInstance();
+        assert(singleton1 == singleton2);
+    }
+    
+    @Test
+    public void verifyThreadSafeUniqueInstance(){
+        ThreadSafeSingleton singleton1 = ThreadSafeSingleton.getInstance();
+        ThreadSafeSingleton singleton2 = ThreadSafeSingleton.getInstance();
+        assert(singleton1 == singleton2);
+    }
+    
+    @Test
+    public void verifyThreadSafeDoubleLockUniqueInstance(){
+        ThreadSafeSingleton singleton1 = ThreadSafeSingleton.getInstanceUsingDoubleLocking();
+        ThreadSafeSingleton singleton2 = ThreadSafeSingleton.getInstanceUsingDoubleLocking();
+        assert(singleton1 == singleton2);
+    }
+}
