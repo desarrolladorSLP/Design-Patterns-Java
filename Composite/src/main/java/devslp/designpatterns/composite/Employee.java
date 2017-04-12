85609a96609c96f48c9715d1184d63d8ad33e03d
Mackaber <mkbravo555@gmail.com>
2017-04-11 20:45:33 -0500

    Falta más docs y alomejor unos tests extras

---
+++ Composite/src/main/java/devslp/designpatterns/composite/Employee.java
@@ -0,0 +1,10 @@
+package devslp.designpatterns.composite;
+
+public interface Employee {
+
+    String getName();
+    String getPosition();
+    double getSalary();
+    double totalDivisionSalary();
+
+}
