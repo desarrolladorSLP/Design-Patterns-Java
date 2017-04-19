8b947a990cd8da592c0323d6331f14645e92fe51
iuresti <iuresti@skytouchtechnology.com>
2017-04-18 21:00:50 -0500

    #2 Fix compile issues

--- TemplateMethod/src/main/java/devslp/designpatterns/TemplateMethod/SortStrategyExample/IntSortHandler.java
+++ TemplateMethod/src/main/java/devslp/designpatterns/TemplateMethod/SortStrategyExample/IntSortHandler.java
@@ -5,33 +5,29 @@ package devslp.designpatterns.TemplateMethod.SortStrategyExample;
   having no dependency whatever on the bubble sort implementation.
 */
 public class IntSortHandler implements SortHandler {
-    private int [] array;
+    private Integer[] array;
 
-    public void setArray(Object [] theArray)
-    {
-        array = new int[theArray.length];
-        for (int i = 0; i < theArray.length ; i++) {
-            array[i] = (int)theArray[i];
-        }
+    public void setArray(Object[] theArray) {
+        array = new Integer[theArray.length];
+
+        System.arraycopy(theArray, 0, array, 0, theArray.length);
     }
 
     public Object[] getArray() {
         return array;
     }
 
-    public int getLength()
-    {
+    public int getLength() {
         return array.length;
     }
 
     public boolean outOfOrder(int index) {
-        return (array[index-1] > array[index]);
+        return (array[index - 1] > array[index]);
     }
 
-    public void swap(int index)
-    {
-        int temp = array[index-1];
-        array[index-1] = array[index];
+    public void swap(int index) {
+        int temp = array[index - 1];
+        array[index - 1] = array[index];
         array[index] = temp;
     }
 }
